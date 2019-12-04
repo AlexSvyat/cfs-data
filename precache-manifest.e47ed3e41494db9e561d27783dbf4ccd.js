88e233ac1d725a77ef401c2bd371df8a38a3f356
Alex Svyatenkiy <alekss@microsoft.com>
2019-12-04 15:59:57 -0800

    Updates

---
+++ precache-manifest.e47ed3e41494db9e561d27783dbf4ccd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9a9f06db12ea8dadac916709b576592",
+    "url": "/cfs-data/index.html"
+  },
+  {
+    "revision": "b38db1dcbdaf83ff4a9e",
+    "url": "/cfs-data/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "b38db1dcbdaf83ff4a9e",
+    "url": "/cfs-data/static/js/2.a3481e6e.chunk.js"
+  },
+  {
+    "revision": "c84b5d27dd317dd35323",
+    "url": "/cfs-data/static/js/main.0382e65f.chunk.js"
+  },
+  {
+    "revision": "9fededefa07f4ae90ca2",
+    "url": "/cfs-data/static/js/runtime-main.3099389a.js"
+  }
+]);
