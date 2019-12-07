66aec0eab60646d2d51869f7acd8c6a040f0b062
Alex Svyatenkiy <alekss@microsoft.com>
2019-12-07 09:59:42 -0800

    Updates

--- precache-manifest.865ec48acd437396604aefb86fa8c381.js
+++ precache-manifest.865ec48acd437396604aefb86fa8c381.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e9a9f06db12ea8dadac916709b576592",
+    "revision": "33ce7a76d5f98efb42981b28eaddb851",
     "url": "/cfs-data/index.html"
   },
   {
-    "revision": "b38db1dcbdaf83ff4a9e",
+    "revision": "0f509bb7ef2165224d43",
     "url": "/cfs-data/static/css/2.64a99b10.chunk.css"
   },
   {
-    "revision": "b38db1dcbdaf83ff4a9e",
-    "url": "/cfs-data/static/js/2.a3481e6e.chunk.js"
+    "revision": "0f509bb7ef2165224d43",
+    "url": "/cfs-data/static/js/2.0e4d47db.chunk.js"
   },
   {
-    "revision": "c84b5d27dd317dd35323",
-    "url": "/cfs-data/static/js/main.0382e65f.chunk.js"
+    "revision": "f369914f00b0acbfa02c",
+    "url": "/cfs-data/static/js/main.2dd23aec.chunk.js"
   },
   {
     "revision": "9fededefa07f4ae90ca2",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cfs-data/precache-manifest.e47ed3e41494db9e561d27783dbf4ccd.js"
+  "/cfs-data/precache-manifest.865ec48acd437396604aefb86fa8c381.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cfs-data/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
